405768078a5059da956615a1415769cac16129cd
Иван <vania.spongebob@gmail.com>
2017-10-07 16:59:05 +0300

    Начало третьей лабораторной работы

--- USBDevicesChecker/USBDevicesChecker/ViewController.m
+++ USBDevicesChecker/USBDevicesChecker/ViewController.m
@@ -7,13 +7,44 @@
 //
 
 #import "ViewController.h"
+#import <IOKit/IOKitLib.h>
+#import <IOKit/usb/IOUSBLib.h>
+#include <IOKit/serial/IOSerialKeys.h>
 
 @implementation ViewController
+
+void SerialDeviceWasAddedFunction(void *refcon, io_iterator_t iterator) {
+    NSLog(@"Hello");
+}
+
+void SerialPortWasRemovedFunction(void *refcon, io_iterator_t iterator) {
+    NSLog(@"Bye");
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
+    IONotificationPortRef notificationPort = IONotificationPortCreate(kIOMasterPortDefault);
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), IONotificationPortGetRunLoopSource(notificationPort), kCFRunLoopDefaultMode);
+    
+    CFMutableDictionaryRef matchingDict = IOServiceMatching(kIOSerialBSDServiceValue);
+    CFRetain(matchingDict); // Need to use it twice and IOServiceAddMatchingNotification() consumes a reference
+    
+    CFDictionaryAddValue(matchingDict, CFSTR(kIOSerialBSDTypeKey), CFSTR(kIOSerialBSDRS232Type));
+    
+    io_iterator_t portIterator = 0;
+    // Register for notifications when a serial port is added to the system
+    kern_return_t result = IOServiceAddMatchingNotification(notificationPort, kIOPublishNotification, matchingDict, SerialDeviceWasAddedFunction, (__bridge void *)(self), &portIterator);
+    while (IOIteratorNext(portIterator)) {}; // Run out the iterator or notifications won't start (you can also use it to iterate the available devices).
+    
+    // Also register for removal notifications
+    IONotificationPortRef terminationNotificationPort = IONotificationPortCreate(kIOMasterPortDefault);
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), IONotificationPortGetRunLoopSource(terminationNotificationPort), kCFRunLoopDefaultMode);
+    result = IOServiceAddMatchingNotification(terminationNotificationPort, kIOTerminatedNotification, matchingDict, SerialPortWasRemovedFunction, (__bridge void *)(self), &portIterator);
+    CFRelease(IONotificationPortGetRunLoopSource(terminationNotificationPort));
+    
+    while (IOIteratorNext(portIterator)) {}; // Run out the iterator or notifications won't start (you can also use it to iterate the available devices).
 }
 
 
