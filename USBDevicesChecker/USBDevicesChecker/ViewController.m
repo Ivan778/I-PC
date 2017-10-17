e04a8b18d6d077d1c90a6c805cb8988d82083c95
Иван <vania.spongebob@gmail.com>
2017-10-17 12:46:15 +0300

    Разработал логику для определения параметров устройства

--- USBDevicesChecker/USBDevicesChecker/ViewController.m
+++ USBDevicesChecker/USBDevicesChecker/ViewController.m
@@ -13,46 +13,202 @@
 
 @implementation ViewController
 
-void SerialDeviceWasAddedFunction(void *refcon, io_iterator_t iterator) {
-    NSLog(@"Hello");
+/*:(NSTimer*)theTimer*/
+
+- (NSMutableString*) removeSpacesInTheBeginning:(NSMutableString*)str {
+    while (true) {
+        if ([str characterAtIndex:0] == ' ') {
+            [str deleteCharactersInRange:NSMakeRange(0, 1)];
+        } else {
+            break;
+        }
+    }
+    
+    return str;
 }
 
-void SerialPortWasRemovedFunction(void *refcon, io_iterator_t iterator) {
-    NSLog(@"Bye");
+- (NSMutableArray*) systemProfiler {
+    FILE *fp;
+    char path[1035];
+    
+    // Команда для чтения
+    fp = popen("system_profiler SPUSBDataType", "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n" );
+        exit(1);
+    }
+    
+    NSMutableArray *devices = [[NSMutableArray alloc] init];
+    NSMutableString *device = [[NSMutableString alloc] init];
+    
+    // Читаем вывод команды
+    while (fgets(path, sizeof(path)-1, fp) != NULL) {
+        device = [NSMutableString stringWithUTF8String:path];
+        device = [self removeSpacesInTheBeginning:device];
+        
+        [devices addObject:device];
+    }
+    
+    // Закрываем файл
+    pclose(fp);
+    
+    return devices;
+}
+
+- (NSMutableArray*) ioreg {
+    FILE *fp;
+    char path[1035];
+    
+    // Команда для чтения
+    fp = popen("ioreg -p IOUSB -w0 | sed 's/[^o]*o //; s/@.*$//' | grep -v '^Root.*'", "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n" );
+        exit(1);
+    }
+    
+    NSMutableArray *devices = [[NSMutableArray alloc] init];
+    NSMutableString *device = [[NSMutableString alloc] init];
+    
+    // Читаем вывод команды
+    while (fgets(path, sizeof(path)-1, fp) != NULL) {
+        device = [NSMutableString stringWithUTF8String:path];
+        [devices addObject:device];
+    }
+    
+    // Закрываем файл
+    pclose(fp);
+    
+    [devices removeObjectAtIndex:0];
+    [devices removeObjectAtIndex:0];
+    
+    return devices;
+}
+
+- (NSMutableArray*) diskutil {
+    FILE *fp;
+    char path[1035];
+    
+    // Команда для чтения
+    fp = popen("diskutil list", "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n" );
+        exit(1);
+    }
+    
+    NSMutableArray *devices = [[NSMutableArray alloc] init];
+    NSMutableString *device = [[NSMutableString alloc] init];
+    
+    // Читаем вывод команды
+    while (fgets(path, sizeof(path)-1, fp) != NULL) {
+        device = [NSMutableString stringWithUTF8String:path];
+        [devices addObject:device];
+    }
+    
+    for (int i = 0; i < 12; i++) {
+        [devices removeObjectAtIndex:0];
+    }
+    
+    // Закрываем файл
+    pclose(fp);
+    
+    return devices;
+}
+
+- (NSMutableArray*) getDeviceDescription: (NSMutableArray*)array: (NSMutableString*)name {
+    NSMutableArray *description = [[NSMutableArray alloc] init];
+    
+    [name insertString:@":" atIndex:[name length] - 1];
+    
+    int i;
+    for (i = 0; [array count]; i++) {
+        if ([name compare:array[i]] == NSOrderedSame) {
+            break;
+        }
+    }
+    
+    i += 2;
+    
+    for (; [array count]; i++) {
+        if ([array[i] compare:@"\n"] == NSOrderedSame) {
+            break;
+        }
+        [description addObject:array[i]];
+    }
+    
+    return description;
+}
+
+// Возвращает основное имя диска (не разделов)
+-(NSMutableString*) rightStringName: (NSMutableString*)str {
+    int i = 3;
+    for (; i < [str length]; i++) {
+        if ([str characterAtIndex:i] == 's') {
+            break;
+        }
+    }
+    
+    NSMutableString *ret = [[NSMutableString alloc] init];
+    [ret setString:[str substringToIndex:i - 1]];
+    
+    return ret;
+}
+
+// Возвращает путь для демонтажа диска
+- (NSMutableString*) giveDiskPath: (NSMutableArray*)description {
+    NSMutableString *returnValue = [[NSMutableString alloc] init];
+    [returnValue setString:@"NOPE"];
+    
+    for (int i = 0; i < [description count]; i++) {
+        if ([description[i] containsString:@"BSD Name"] == YES) {
+            [returnValue setString:[description[i] substringFromIndex:10]];
+            
+            returnValue = [self rightStringName:returnValue];
+            [returnValue insertString:@"/dev/" atIndex:0];
+            
+            return returnValue;
+        }
+    }
+        
+    return returnValue;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
-    IONotificationPortRef notificationPort = IONotificationPortCreate(kIOMasterPortDefault);
-    CFRunLoopAddSource(CFRunLoopGetCurrent(), IONotificationPortGetRunLoopSource(notificationPort), kCFRunLoopDefaultMode);
     
-    CFMutableDictionaryRef matchingDict = IOServiceMatching(kIOSerialBSDServiceValue);
-    CFRetain(matchingDict); // Need to use it twice and IOServiceAddMatchingNotification() consumes a reference
+    //NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
+    NSMutableArray *sp = [self systemProfiler];
+    NSMutableArray *io = [self ioreg];
+    NSMutableArray *du = [self diskutil];
     
-    CFDictionaryAddValue(matchingDict, CFSTR(kIOSerialBSDTypeKey), CFSTR(kIOSerialBSDRS232Type));
+    for (int i = 0; i < [du count]; i++) {
+        NSLog(@"%@", du[i]);
+    }
     
-    io_iterator_t portIterator = 0;
-    // Register for notifications when a serial port is added to the system
-    kern_return_t result = IOServiceAddMatchingNotification(notificationPort, kIOPublishNotification, matchingDict, SerialDeviceWasAddedFunction, (__bridge void *)(self), &portIterator);
-    while (IOIteratorNext(portIterator)) {}; // Run out the iterator or notifications won't start (you can also use it to iterate the available devices).
+    for (int i = 0; i < [io count]; i++) {
+        NSLog(@"%@", io[i]);
+    }
     
-    // Also register for removal notifications
-    IONotificationPortRef terminationNotificationPort = IONotificationPortCreate(kIOMasterPortDefault);
-    CFRunLoopAddSource(CFRunLoopGetCurrent(), IONotificationPortGetRunLoopSource(terminationNotificationPort), kCFRunLoopDefaultMode);
-    result = IOServiceAddMatchingNotification(terminationNotificationPort, kIOTerminatedNotification, matchingDict, SerialPortWasRemovedFunction, (__bridge void *)(self), &portIterator);
-    CFRelease(IONotificationPortGetRunLoopSource(terminationNotificationPort));
+    NSMutableArray *devices = [[NSMutableArray alloc] init];
+    for (int i = 0; i < [io count]; i++) {
+        [devices addObject:[self getDeviceDescription:sp :io[i]]];
+    }
     
-    while (IOIteratorNext(portIterator)) {}; // Run out the iterator or notifications won't start (you can also use it to iterate the available devices).
+    NSLog(@"%@", [self giveDiskPath:devices[0]]);
+    
+    /*
+    NSRange range = [du[1] rangeOfString:@"NAME"];
+    NSLog(@"%lu", (unsigned long)range.location);
+    
+    int i = (int)range.location;
+    for (; i < [du[3] length]; i++) {
+        if ([du[3] characterAtIndex:i] == ' ') break;
+    }
+    
+    NSString *s = [du[3] substringWithRange:NSMakeRange(range.location, i - range.location)];
+    
+    NSLog(@"%@", s);
+     */
+    
 }
-
-
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
-}
-
 
 @end
